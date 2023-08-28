4c96ff53592c5abe8e2c5a6c38ff96aa7a2f8441
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-08-28 15:12:27 -0300

    Hipotenusa2.java
    adicionados comentários no código

--- src/main/java/com/mycompany/hipotenusa/Hipotenusa2.java
+++ src/main/java/com/mycompany/hipotenusa/Hipotenusa2.java
@@ -1,13 +1,16 @@
 package com.mycompany.hipotenusa;
 
 public class Hipotenusa2 {
+    // variáveis para a hipotenusa, catetos A e B.
     public double a, b, hip;
+    // definir que cateto A e B serão elevados ao quadrado e que a hipotenusa receberá a soma dos dois.
     public double calculo(){
         a = Math.pow(a, 2);
         b = Math.pow(b, 2);
         hip = a + b;
         return hip;
     }
+    // tendo realizado a soma, para obter o real resultado da hipotenusa, será feita a raiz quadrada da hipotenusa.
     public void valorhip(){
         calculo();
         System.out.println("A hipotenusa é: "+ Math.sqrt(hip));
