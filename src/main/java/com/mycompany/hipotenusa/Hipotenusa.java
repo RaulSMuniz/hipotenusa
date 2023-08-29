1ea5daac88333a7a6e6d1b6b2637d79ee2d4abc7
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-08-28 21:51:08 -0300

    Hipotenusa.java
    adicionados scanners

--- src/main/java/com/mycompany/hipotenusa/Hipotenusa.java
+++ src/main/java/com/mycompany/hipotenusa/Hipotenusa.java
@@ -1,16 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.hipotenusa;
-
+import java.util.Scanner;
 public class Hipotenusa {
-
     public static void main(String[] args) {
         Hipotenusa2 res = new Hipotenusa2();
-        res.a = 6;
-        res.b = 8;
+        
+        Scanner cateto01 = new Scanner(System.in);
+        System.out.println("Qual o valor do primeiro cateto?");
+        res.a = (cateto01.nextInt());
+        
+        Scanner cateto02 = new Scanner(System.in);
+        System.out.println("Qual o valor do segundo cateto?");
+        res.b = (cateto02.nextInt());
         res.valorhip();
+        cateto01.close();
+        cateto02.close();
          
     }
 }
